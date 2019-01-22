507b86a541d9eefe5a95bec13166f59fd48dad86
MrCliff <j_1.kallio@hotmail.com>
2019-01-22 20:50:58 +0200

    Change the IDE to PlatformIO (an Atom plugin)
    Add a fast forward button.
    Make the basic functionality work.
    Add some debug prints.

--- src/Led.cpp
+++ src/Led.cpp
@@ -1,47 +1,132 @@
 #include <Arduino.h>
 #include <stdint.h>
+#include <math.h>
 
-
-enum PwmBoundaryValues {
-    kLow = 0,
-    kHigh = UINT8_MAX
+/**
+ * Represents the boundary values of a PWM pin on an Arduino board.
+ */
+enum PwmBoundaryValues : uint8_t {
+  kLow = 0,
+  kHigh = UINT8_MAX
 };
+
+
+/**
+ * Represents the boundary values of a digital pin on an Arduino board.
+ */
+enum DigitalBoundaryValues : uint8_t {
+  kLow = LOW,
+  kHigh = HIGH
+};
+
+
+/**
+ * Represents one LED connected to a digital pin.
+ */
+class Led {
+public:
+  Led(uint8_t pinNumber, DigitalBoundaryValues offValue) :
+    pinNumber(pinNumber), offValue(offValue) {
+  }
+
+  void begin() {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, offValue);
+  }
+
+  void setValue(uint8_t val) {
+    currentValue = constrain(
+      val, DigitalBoundaryValues::kLow, DigitalBoundaryValues::kHigh
+    );
+    writeValue();
+  }
+
+  void setValue(double val) {
+    double constrVal = constrain(val, 0, 1);
+    currentValue = (uint8_t)lround(
+      constrVal * DigitalBoundaryValues::kHigh + DigitalBoundaryValues::kLow
+    );
+    writeValue();
+  }
+
+  uint8_t getValue() const {
+    return currentValue;
+  }
+
+  /**
+   * Writes the current value to the output.
+   */
+  void writeValue() const {
+    switch (offValue) {
+      case DigitalBoundaryValues::kHigh:
+        digitalWrite(pinNumber, DigitalBoundaryValues::kHigh - currentValue);
+        break;
+      case DigitalBoundaryValues::kLow:
+        digitalWrite(pinNumber, currentValue - DigitalBoundaryValues::kLow);
+        break;
+    }
+  }
+
+private:
+  const DigitalBoundaryValues offValue;
+  const uint8_t pinNumber;
+
+  uint8_t currentValue;
+};
+
 
 /**
  * Represents one LED connected to a PWM pin.
  */
 class PwmLed {
 public:
-    PwmLed(int pinNumber, PwmBoundaryValues offValue) : pinNumber(pinNumber), offValue(offValue) {
-        pinMode(pinNumber, OUTPUT);
-        analogWrite(pinNumber, offValue);
-    }
+  PwmLed(uint8_t pinNumber, PwmBoundaryValues offValue) :
+    pinNumber(pinNumber), offValue(offValue) {
+  }
 
-    void setValue(uint8_t val) {
-        currentValue = val;
-    }
+  void begin() {
+    pinMode(pinNumber, OUTPUT);
+    analogWrite(pinNumber, offValue);
+  }
 
-    uint8_t getValue() const {
-        return currentValue;
-    }
+  void setValue(uint8_t val) {
+    currentValue = constrain(
+      val, PwmBoundaryValues::kLow, PwmBoundaryValues::kHigh
+    );
+    writeValue();
+  }
 
-    /**
-     * Writes the current value to the output.
-     */
-    void writeValue() const {
-        switch (offValue) {
-            case kHigh:
-                analogWrite(pinNumber, kHigh - currentValue);
-            case kLow:
-                analogWrite(pinNumber, currentValue - kLow);
-        }
+  void setValue(double val) {
+    double constrVal = constrain(val, 0, 1);
+    currentValue = (uint8_t)lround(
+      constrVal * PwmBoundaryValues::kHigh + PwmBoundaryValues::kLow
+    );
+    writeValue();
+  }
+
+  uint8_t getValue() const {
+    return currentValue;
+  }
+
+  /**
+   * Writes the current value to the output.
+   */
+  void writeValue() const {
+    switch (offValue) {
+      case PwmBoundaryValues::kHigh:
+        analogWrite(pinNumber, PwmBoundaryValues::kHigh - currentValue);
+        break;
+      case PwmBoundaryValues::kLow:
+        analogWrite(pinNumber, currentValue - PwmBoundaryValues::kLow);
+        break;
     }
+  }
 
 private:
-    const PwmBoundaryValues offValue;
-    const int pinNumber;
+  const PwmBoundaryValues offValue;
+  const uint8_t pinNumber;
 
-    uint8_t currentValue;
+  uint8_t currentValue;
 };
 
 
@@ -50,33 +135,74 @@ private:
  */
 class DualPwmLed {
 public:
-    DualPwmLed(int led1Pin, int led2Pin, PwmBoundaryValues offValue) :
-            DualPwmLed(led1Pin, led2Pin, offValue, offValue) {
-    }
+  DualPwmLed(uint8_t led1Pin, uint8_t led2Pin, PwmBoundaryValues offValue) :
+    DualPwmLed(led1Pin, led2Pin, offValue, offValue) {
+  }
 
-    DualPwmLed(int led1Pin, int led2Pin, PwmBoundaryValues offValue1, PwmBoundaryValues offValue2) :
-            led1(led1Pin, offValue1), led2(led2Pin, offValue2) {
-    }
+  DualPwmLed(uint8_t led1Pin, uint8_t led2Pin, PwmBoundaryValues offValue1,
+    PwmBoundaryValues offValue2) :
+    led1(led1Pin, offValue1), led2(led2Pin, offValue2) {
+  }
 
-    void setTwoPoleValue(uint8_t val) {
-        led1.setValue(val);
-        led2.setValue(kHigh - val);
-    }
+  void begin() {
+    led1.begin();
+    led2.begin();
+  }
 
-    uint8_t getLed1Value() const {
-        return led1.getValue();
-    }
+  void setTwoPoleValue(uint8_t val) {
+    led1.setValue(val);
+    led2.setValue((uint8_t)(PwmBoundaryValues::kHigh - val));
+  }
 
-    uint8_t getLed2Value() const {
-        return led2.getValue();
-    }
+  void setTwoPoleValue(double val) {
+    double constrVal = constrain(val, 0, 1);
+    led1.setValue(constrVal);
+    led2.setValue(1.0 - constrVal);
+  }
 
-    void writePins() const {
-        led1.writeValue();
-        led2.writeValue();
-    }
+  uint8_t getLed1Value() const {
+    return led1.getValue();
+  }
+
+  uint8_t getLed2Value() const {
+    return led2.getValue();
+  }
+
+  void writeValue() const {
+    led1.writeValue();
+    led2.writeValue();
+  }
 
 private:
-    PwmLed led1;
-    PwmLed led2;
+  PwmLed led1;
+  PwmLed led2;
+};
+
+
+/**
+ * Holds a value that can be changed in an accelerating manner.
+ */
+class Accelerator {
+public:
+  Accelerator(double rate) : rate(rate) {
+  }
+
+  void accelerate() {
+    acceleration += rate;
+    value += acceleration;
+  }
+
+  void reset() {
+    acceleration = 0.0;
+    value = 0.0;
+  }
+
+  double getValue() const {
+    return value;
+  }
+
+private:
+  const double rate;
+  double acceleration;
+  double value;
 };
--- src/MedicineReminder.ino
+++ src/MedicineReminder.ino
@@ -2,31 +2,84 @@
 #include <stdint.h>
 #include "Led.cpp"
 
-const int kPinLedR = 9;
-const int kPinLedG = 10;
-const int kPinLedB = 11;
-const int kPinBtn = 8;
+const uint8_t kPinPWMLedR = 9;
+const uint8_t kPinPWMLedG = 10;
+const uint8_t kPinLedB = 11;
+const uint8_t kPinInterruptBtn = 2;
+const uint8_t kPinRecordBtn = 8;
+const uint8_t kPinFastForwardBtn = 7;
 
-DualPwmLed *dualLedRG;
-PwmLed *ledB;
+// Arduino handles integers as 16 bit values, so 12 hours (43200s) would
+// overflow and become a negative value, if there wasn't a type cast.
+const uint32_t kShiftTimeSec = (uint32_t)60 * 60 * 12;
 
-uint8_t ledRGValue = 0;
-uint8_t switchSpeed = 1;
+DualPwmLed dualLedRG = DualPwmLed(kPinPWMLedG, kPinPWMLedR, PwmBoundaryValues::kHigh);
+PwmLed ledB = Led(kPinLedB, DigitalBoundaryValues::kHigh);
+
+uint32_t lastResetTs = 0;
+
+volatile bool resetTsFlag = false;
+
+Accelerator ffAccelerator(2.0);
 
 void setup() {
-    // put your setup code here, to run once:
-    dualLedRG = new DualPwmLed(kPinLedG, kPinLedR, PwmBoundaryValues::kHigh);
-    ledB = new PwmLed(kPinLedB, PwmBoundaryValues::kHigh);
+  dualLedRG.begin();
+  ledB.begin();
 
-    pinMode(kPinBtn, INPUT);
+  rtc.begin();
+  lastResetTs = rtc.now().getEpoch();
+
+  Serial.begin(115200);
+
+  // Buttons
+  pinMode(kPinRecordBtn, INPUT);
+  pinMode(kPinFastForwardBtn, INPUT);
+  attachInterrupt(digitalPinToInterrupt(kPinInterruptBtn), resetTs, RISING);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    ledRGValue += switchSpeed;
+  DateTime now = rtc.now(); // get the current date-time
+  uint32_t ts = now.getEpoch();
 
-    dualLedRG->setTwoPoleValue(ledRGValue);
-    dualLedRG->writePins();
+  // Reset
+  if (resetTsFlag) {
+    lastResetTs = ts;
 
-    delay(10);
+    resetTsFlag = false;
+  }
+
+  // Fast forward
+  if (digitalRead(kPinFastForwardBtn)) {
+    ffAccelerator.accelerate();
+    lastResetTs = (uint32_t)constrain((double)lastResetTs - ffAccelerator.getValue(), (double)0, (double)ts);
+  }
+  else {
+    ffAccelerator.reset();
+  }
+
+  lastResetTs = (uint32_t)constrain(lastResetTs, (uint32_t)0, ts);
+  double ledRGValue = 1.0 - ((double)(ts - lastResetTs) / kShiftTimeSec);
+  dualLedRG.setTwoPoleValue(ledRGValue);
+  Serial.print("LedValue: ");
+  Serial.print(ledRGValue);
+  Serial.print("; Epoch: ");
+  Serial.print(ts);
+  Serial.print("; lastResetEpoch: ");
+  Serial.print(lastResetTs);
+  Serial.print("; kShiftTimeSec: ");
+  Serial.print(kShiftTimeSec);
+  Serial.print("; (uint32_t)60 * 60 * 12: ");
+  Serial.print((uint32_t)60 * 60 * 12);
+  Serial.print("; ts - lastResetTs: ");
+  Serial.print(ts - lastResetTs);
+  Serial.print("; ts - lastResetTs / kShiftTimeSec: ");
+  Serial.print((double)(ts - lastResetTs) / kShiftTimeSec);
+  Serial.print("; ffSpeed: ");
+  Serial.println(ffAccelerator.getValue());
+
+  delay(100);
+}
+
+void resetTs() {
+  resetTsFlag = true;
 }
